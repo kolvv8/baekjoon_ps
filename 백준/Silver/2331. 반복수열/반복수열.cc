76f56b195c9f890097ac4969445244e38caf6831
kolvv8 <121375953+kolvv8@users.noreply.github.com>
2025-11-02 22:06:31 +0900

    [Silver IV] Title: 반복수열, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.cc
+++ 백준/Silver/2331. 반복수열/반복수열.cc
@@ -1,34 +1,52 @@
 #include <iostream>
-#include <string>
+#include <unordered_set>
 #include <cmath>
+#include <map>
+#include <algorithm>
 using namespace std;
 
-int main()
-{
-    string a;
-    int p,num;
-    int cnt=0;
-    string c[10000];
-    
-    cin>>a>>p;
-    c[0]=a;
-    
-    while(1){
-        
-        num=0;
-        for(int i=0;i<c[cnt].size();i++){
-            num+=pow((c[cnt][i]-'0'),p);
-        }
-        cnt++;
-        c[cnt]=to_string(num);
-        for(int i=0;i<cnt;i++){
-            if(c[i]==c[cnt]){
-                cout<<i;
-                return 0;
-            }
+// 다음 A 값을 반환해주는 함수
+int func(int x, int p) {
+    int res = 0;
+    while (x > 0) {
+        int digit = x % 10;
+        res += (int)pow(digit, p);
+        x /= 10;
+    }
+    return res;
+}
+
+
+int main() {
+
+    // map -> key: D[A] 값, value: 몇 번째로 나왔는지 기록
+    map<int, long long> m;
+
+    // A: 초기 A 입력값(나중에 A가 바뀔 때 마다 A 값 업데이트 할 예장), P: 몇 번 곱할 건지 입력값
+    int A, P;
+    cin >> A >> P;
+
+    // 몇 번째 값인지 나타내는 변수, 시작은 0부터
+    long long cnt = 0;
+
+    while (true) {
+        // 이전 값이 반복된 경우
+        if (m.find(A) != m.end()) {
+
+            cout << m[A] << "\n";
+            return 0;
+
         }
 
+        // key: A값, value: 몇 번째 인지 기록
+        m[A] = cnt;
+
+        // 다음 횟수
+        cnt++;
+
+        // 다음 A 값 지정
+        A = func(A, P);
+
+
     }
-    
-    return 0;
 }
